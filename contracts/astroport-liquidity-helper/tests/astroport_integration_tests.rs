aee1a2a3e879d6819f735f45bcb9d282aea950ac
Sturdy <91910406+apollo-sturdy@users.noreply.github.com>
2024-08-21 14:48:01 +0200

    chore: bump cw-dex versions

--- contracts/astroport-liquidity-helper/tests/astroport_integration_tests.rs
+++ contracts/astroport-liquidity-helper/tests/astroport_integration_tests.rs
@@ -423,7 +423,9 @@ pub fn test_balancing_provide_liquidity(
             AssetInfo::native("uluna".to_string()),
             AssetInfo::cw20(Addr::unchecked(&astro_token)),
         ],
-        liquidity_manager: Addr::unchecked(astroport_contracts.liquidity_manager.address.clone()),
+        liquidity_manager: Some(Addr::unchecked(
+            astroport_contracts.liquidity_manager.address.clone(),
+        )),
     };
 
     // Increase allowance of astro token for Pair contract
